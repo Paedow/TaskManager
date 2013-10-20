bcbf2b04cf8f16f2863bfa16a0a2d7dbe5826953
dr4yyee <eingutespasswortist123456@yahoo.de>
2013-10-20 17:39:59 +0200

    unicode support :D

--- rptm/rptm/MessageServer.cs
+++ rptm/rptm/MessageServer.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Net;
 using System.Web;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace rptm
 {
@@ -24,7 +25,7 @@ namespace rptm
                 WebClient wClient = new WebClient();
                 try
                 {
-                    string debugInfo = wClient.DownloadString(apiString + "sendMessage.php?user=" + user + "&message=" + HttpUtility.UrlEncode(message));
+                    string debugInfo = wClient.DownloadString(apiString + "sendMessage.php?user=" + user + "&message=" + message.To64());
                 }
                 catch (WebException ex)
                 {
@@ -36,21 +37,31 @@ namespace rptm
 
         public string LoadMessages()
         {
-            string lol = "";
             if (!offline)
             {
                 WebClient wClient = new WebClient();
                 try
                 {
-                    lol = wClient.DownloadString(apiString + "getMessages.php");
+                    var r = wClient.DownloadString(apiString + "getMessages.php");
+                    List<string> list = new List<string>();
+                    foreach (string row in r.Replace("\r", "").Split('\n'))
+                    {
+                        string[] b = row.Split(':');
+                        var v = ((b.Count() > 1) ? String.Join(":", b.Except(new[] { b.First() }).ToArray()).From64() : "");
+                        string s = string.Format("{0}:{1}\n", b.First(), v);
+                        list.Add(s);
+                    }
+                    return r.Contains(":") ? String.Join("\n", list.ToArray()).Replace("\n", "\n\r") : r.From64();
                 }
                 catch (WebException ex)
                 {
                     offline = true;
                     MessageBox.Show("Die Verbindung zum Internet konnte nicht hergestellt werden!", "Verbindungsfehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return ex.ToString();
                 }
             }
-            return lol;
+
+            return ""; //Just to make the compiler happy
         }
     }
 }
--- rptm/rptm/Program.cs
+++ rptm/rptm/Program.cs
@@ -14,7 +14,7 @@ namespace rptm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
